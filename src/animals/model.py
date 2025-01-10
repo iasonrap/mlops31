fa0d01b45b67ce1cba9b51335e4afea1633355af
asbjornhp <s204084@student.dtu.dk>
2025-01-10 15:52:55 +0100

    Added a model to model.py

---
+++ src/animals/model.py
@@ -0,0 +1,20 @@
+import torch
+import timm
+import torch.nn as nn
+
+class AnimalModel(nn.Module):
+    def __init__(self, num_classes):
+        super(AnimalModel, self).__init__()
+        self.model = timm.create_model('resnet18', pretrained=True)
+        in_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+if __name__ == '__main__':
+    model = AnimalModel(10)
+    x = torch.randn(1, 3, 224, 224)
+    y = model(x)
+    print(y.shape)
