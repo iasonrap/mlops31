4c58926db700038e111064f834d47347c59f0d40
Joe <joelfarrecortes@gmail.com>
2025-01-16 11:30:35 +0100

    config applied to model.py

--- src/animals/model.py
+++ src/animals/model.py
@@ -1,14 +1,17 @@
+import hydra
 import torch
 import timm
 import torch.nn as nn
 
+@hydra.main(config_name="conf/config.yaml")
 class AnimalModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, cfg):
         """Initialize the model with the number of classes"""
         super(AnimalModel, self).__init__()
-        self.model = timm.create_model('resnet18', pretrained=True)
+        self.model = timm.create_model(cfg.hyperparameters.model_name, pretrained=True)
         in_features = self.model.fc.in_features
-        self.model.fc = nn.Linear(in_features, num_classes)
+        self.model.fc = nn.Linear(in_features, cfg.hyperparameters.num_classes)
+        print(cfg.hyperparameters.model_name)
 
     def forward(self, x):
         return self.model(x)
