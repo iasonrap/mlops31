ed654619285473171f9949814c67fb531148291c
asbjornhp <s204084@student.dtu.dk>
2025-01-24 10:24:43 +0100

    ruffed it to pass the ruff checks zzz

--- src/animals/profiling.py
+++ src/animals/profiling.py
@@ -1,9 +1,9 @@
 import pstats
 
 # Load the .prof file
-stats = pstats.Stats('profile_results2.prof')
+stats = pstats.Stats("profile_results2.prof")
 
 # Sort and print the statistics
-stats.strip_dirs()           # Remove extraneous path information
-stats.sort_stats('cumtime')     # Sort by time (or 'cumulative', 'calls', etc.)
-stats.print_stats(20)        # Print the top 20 time-consuming functions
+stats.strip_dirs()  # Remove extraneous path information
+stats.sort_stats("cumtime")  # Sort by time (or 'cumulative', 'calls', etc.)
+stats.print_stats(20)  # Print the top 20 time-consuming functions
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,12 @@
-import pytest
-from fastapi.testclient import TestClient
-from src.animals.api import app  # Import FastAPI app from api.py
 import os
 
+import pytest
+from fastapi.testclient import TestClient
+
+from src.animals.api import app  # Import FastAPI app from api.py
+
 client = TestClient(app)  # TestClient allows us to make requests to the API
+
 
 def test_hello_world():
     """Test that the / route returns 'Hello from the backend!'"""
@@ -11,32 +14,32 @@ def test_hello_world():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from the backend!"}
 
-@pytest.mark.skipif(not os.path.exists("data/raw/raw-img/cane/OIF-e2bexWrojgtQnAPPcUfOWQ.jpeg"), reason="Test image not found")
+
+@pytest.mark.skipif(
+    not os.path.exists("data/raw/raw-img/cane/OIF-e2bexWrojgtQnAPPcUfOWQ.jpeg"), reason="Test image not found"
+)
 def test_classify_endpoint():
     """Test that the /classify endpoint works by sending a mock image"""
     with open("data/raw/raw-img/cane/OIF-e2bexWrojgtQnAPPcUfOWQ.jpeg", "rb") as image_file:
-        response = client.post(
-            "/classify/",
-            files={"file": ("test_image.jpg", image_file, "image/jpeg")}
-        )
+        response = client.post("/classify/", files={"file": ("test_image.jpg", image_file, "image/jpeg")})
     assert response.status_code == 200
     assert "prediction" in response.json()  # Assuming the response contains a 'prediction' key
+
 
 @pytest.mark.skipif(not os.path.exists("data/raw/raw-img/cane"), reason="Test data not found")
 def test_post_data_endpoint():
     """Test that the /post_data endpoint uploads data correctly."""
     mock_data = {"key": "value"}
-    response = client.post(
-        "/post_data",
-        json=mock_data
-    )
+    response = client.post("/post_data", json=mock_data)
     assert response.status_code == 200
     assert response.json() == {"status": "success"}  # Example response, adjust as needed
+
 
 def main():
     test_hello_world()
     test_classify_endpoint()
     test_post_data_endpoint()
+
 
 if __name__ == "__main__":
     main()
